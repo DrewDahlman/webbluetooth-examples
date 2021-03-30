d6aca748b089567fa57d417ffb3cbab994abbe6c
Drew Dahlman <info@drewdahlman.com>
2021-03-30 07:40:39 -0400

    reading battery level

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,10 +4,44 @@ const Bluetooth = {
   init: async () => {
     let device = null;
 
+    // Define service
+    const batteryServiceUUID = "0000180f-0000-1000-8000-00805f9b34fb";
+    const batteryCharacteristicUUID = "00002a19-0000-1000-8000-00805f9b34fb";
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Bluetooth/requestDevice
+    // Make use of filters to select specific device(s)
     device = await navigator.bluetooth.requestDevice({
-      acceptAllDevices: true, // Connect to anything
+      filters: [{ name: "iPad" }], // Filter by name
+      optionalServices: [batteryServiceUUID], // Specify services
     });
-    console.log(device);
+
+    // Connect to gatt server
+    const server = await device.gatt.connect();
+    console.log("Connected to server");
+
+    // Get a service
+    const batteryService = await server.getPrimaryService(batteryServiceUUID);
+    console.log(batteryService);
+
+    // Get charactistics
+    const batteryCharacteristic = await batteryService.getCharacteristic(
+      batteryCharacteristicUUID
+    );
+
+    // Read the characteristic
+    const batteryValue = await batteryCharacteristic.readValue();
+
+    // Read the value & Log
+    console.log("> Battery Level is " + batteryValue.getUint8(0) + "%");
+
+    // If we wanted to we could listen to the battery
+    batteryCharacteristic.addEventListener(
+      "characteristicvaluechanged",
+      (event) => {
+        const value = event.target.value;
+        console.log("> Battery Level is " + batteryValue.getUint8(0) + "%");
+      }
+    );
   },
 };
 

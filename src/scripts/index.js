c31278e2d9578a8ee53f7a35ba5ffe15a1575145
Drew Dahlman <info@drewdahlman.com>
2021-03-30 09:08:25 -0400

    walk through parsing patterns and reverse engineer messages and the meanings

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,25 +4,59 @@ const handleNotifications = (event) => {
   const value = event.target.value;
   const utf8decoder = new TextDecoder();
 
-  console.log(value);
+  // 1. Look at messages again
+  // Put Length & hex on same line for easy parsing
+  let a = [];
+  for (let i = 0; i < value.byteLength; i++) {
+    let val = ("00" + value.getUint8(i).toString(16)).slice(-2);
+    a.push(val);
+  }
+  console.log(`${value.byteLength}: ${a.join(" ")}`);
 
-  // 1. Convert & parse
-  // Result: Gibberish & nonsense
-  // if( value.byteLength == 8 ){
-  //     const eventInt8Array = new Int8Array(value.buffer);
-  //     const eventData = utf8decoder.decode(eventInt8Array);
-  //     console.log(eventData);
-  //     console.log(value.getUint8(0))
-  // }
+  // 2. Find patterns
+  // Single Tap
+  // Double Tap
+  // Triple Tap
+  // Long Hold Start
+  // Message
+  // Long Hold End
+  // Long Hold StartB
+  // Message
+  // Long Hold EndB
 
-  // 2. Try decode method
-  // let a = [];
-  // for (let i = 0; i < value.byteLength; i++) {
-  //     let val = ("00" + value.getUint8(i).toString(16)).slice(-2);
-  //     a.push(val);
-  // }
-  // console.log(`Length: ${value.byteLength}`);
-  // console.log(`Name Hex: ${a.join(" ")}`);
+  // 3. Trim to match patterns
+  // For 8 our patterns start at index 6, so splice everything from 5 to the end
+  // For 32 the pattern starts at index 13, so cut from 13 + 3
+  //   let output;
+  //   if (a.length == 8) output = a.splice(5, a.length - 1).join("");
+  //   if (a.length == 32) output = a.splice(13, 3).join("");
+
+  //   switch (output) {
+  //     case "30d4dd":
+  //       console.log("Noise Canceling Enabled");
+  //       break;
+  //     case "11c4dd":
+  //       console.log("Noise Canceling Disabled");
+  //       break;
+  //     case "2c1320":
+  //       console.log("Right ear Single tap");
+  //       break;
+  //     case "2c1340":
+  //       console.log("Right ear double tap");
+  //       break;
+  //     case "2c1360":
+  //       console.log("Right ear tripple click");
+  //       break;
+  //     case "2c1380":
+  //       console.log("Right ear long hold complete");
+  //       break;
+  //     case "2c13a0":
+  //       console.log("Right ear long hold start");
+  //       break;
+  //     default:
+  //       console.log(`Unknown command: ${output}`);
+  //       break;
+  //   }
 };
 
 const Bluetooth = {

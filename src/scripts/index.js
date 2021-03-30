5fa989a9c6c1f97a3be4d79e43805a755579ca09
Drew Dahlman <info@drewdahlman.com>
2021-03-30 18:18:27 -0400

    cleanup

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,6 @@ import "regenerator-runtime/runtime";
 
 const handleNotifications = (event) => {
   const value = event.target.value;
-  const utf8decoder = new TextDecoder();
 
   // 1. Look at messages again
   // Put Length & hex on same line for easy parsing
@@ -11,7 +10,7 @@ const handleNotifications = (event) => {
     let val = ("00" + value.getUint8(i).toString(16)).slice(-2);
     a.push(val);
   }
-  console.log(`${value.byteLength}: ${a.join(" ")}`);
+  //   console.log(`${value.byteLength}: ${a.join(" ")}`);
 
   // 2. Find patterns
   // Single Tap
@@ -25,38 +24,38 @@ const handleNotifications = (event) => {
   // Long Hold EndB
 
   // 3. Trim to match patterns
-  // For 8 our patterns start at index 6, so splice everything from 5 to the end
+  // For 8 our patterns start at index 5, so splice everything from 5 to the end
   // For 32 the pattern starts at index 13, so cut from 13 + 3
-  //   let output;
-  //   if (a.length == 8) output = a.splice(5, a.length - 1).join("");
-  //   if (a.length == 32) output = a.splice(13, 3).join("");
+  // let output;
+  // if (a.length == 8) output = a.splice(5, a.length - 1).join("");
+  // if (a.length == 32) output = a.splice(13, 3).join("");
 
-  //   switch (output) {
-  //     case "30d4dd":
-  //       console.log("Noise Canceling Enabled");
-  //       break;
-  //     case "11c4dd":
-  //       console.log("Noise Canceling Disabled");
-  //       break;
-  //     case "2c1320":
-  //       console.log("Right ear Single tap");
-  //       break;
-  //     case "2c1340":
-  //       console.log("Right ear double tap");
-  //       break;
-  //     case "2c1360":
-  //       console.log("Right ear tripple click");
-  //       break;
-  //     case "2c1380":
-  //       console.log("Right ear long hold complete");
-  //       break;
-  //     case "2c13a0":
-  //       console.log("Right ear long hold start");
-  //       break;
-  //     default:
-  //       console.log(`Unknown command: ${output}`);
-  //       break;
-  //   }
+  // switch (output) {
+  //   case "30d4dd":
+  //     console.log("Noise Canceling Enabled");
+  //     break;
+  //   case "11c4dd":
+  //     console.log("Noise Canceling Disabled");
+  //     break;
+  //   case "2c1320":
+  //     console.log("Right ear Single tap");
+  //     break;
+  //   case "2c1340":
+  //     console.log("Right ear double tap");
+  //     break;
+  //   case "2c1360":
+  //     console.log("Right ear tripple tap");
+  //     break;
+  //   case "2c1380":
+  //     console.log("Right ear long hold complete");
+  //     break;
+  //   case "2c13a0":
+  //     console.log("Right ear long hold start");
+  //     break;
+  //   default:
+  //     console.log(`Unknown command: ${output}`);
+  //     break;
+  // }
 };
 
 const Bluetooth = {

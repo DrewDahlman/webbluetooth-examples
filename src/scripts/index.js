257bc44a89defb148affe5c07654131d3d3484b0
Drew Dahlman <info@drewdahlman.com>
2021-03-30 08:43:58 -0400

    setup service & characteristics

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,73 +1,53 @@
 import "regenerator-runtime/runtime";
 
+const handleNotifications = (event) => {
+  const value = event.target.value;
+  const utf8decoder = new TextDecoder();
+
+  console.log(value);
+
+  // 1. Convert & parse
+  // Result: Gibberish & nonsense
+  // if( value.byteLength == 8 ){
+  //     const eventInt8Array = new Int8Array(value.buffer);
+  //     const eventData = utf8decoder.decode(eventInt8Array);
+  //     console.log(eventData);
+  //     console.log(value.getUint8(0))
+  // }
+
+  // 2. Try decode method
+  // let a = [];
+  // for (let i = 0; i < value.byteLength; i++) {
+  //     let val = ("00" + value.getUint8(i).toString(16)).slice(-2);
+  //     a.push(val);
+  // }
+  // console.log(`Length: ${value.byteLength}`);
+  // console.log(`Name Hex: ${a.join(" ")}`);
+};
+
 const Bluetooth = {
   init: async () => {
-    let device = null;
-
-    // Define service
-    const unknownServiceServiceUUID = "0000180a-0000-1000-8000-00805f9b34fb";
-    const unknownServiceCharacteristicUUID =
-      "00002a24-0000-1000-8000-00805f9b34fb";
-
-    // https://developer.mozilla.org/en-US/docs/Web/API/Bluetooth/requestDevice
-    // Make use of filters to select specific device(s)
-    device = await navigator.bluetooth.requestDevice({
-      filters: [{ name: "iPad" }], // Filter by name
-      optionalServices: [unknownServiceServiceUUID], // Specify services
+    const serviceUUID = "a7a473e9-19c6-491b-aea6-7ea92b8f043a";
+    const characteristicUUID = "a7a48322-19c6-491b-aea6-7ea92b8f043a";
+    const device = await navigator.bluetooth.requestDevice({
+      filters: [{ namePrefix: "Galaxy Buds Live" }],
+      optionalServices: [serviceUUID],
     });
 
-    // Connect to gatt server
     const server = await device.gatt.connect();
-    console.log("Connected to server");
+    const service = await server.getPrimaryService(serviceUUID);
+    const characteristic = await service.getCharacteristic(characteristicUUID);
 
-    // Get a service
-    const unknownServiceService = await server.getPrimaryService(
-      unknownServiceServiceUUID
+    await characteristic.startNotifications();
+    console.log("Starting Notifications");
+
+    characteristic.addEventListener(
+      "characteristicvaluechanged",
+      handleNotifications
     );
-    console.log(unknownServiceService);
-
-    // Get charactistics
-    const unknownServiceCharacteristic = await unknownServiceService.getCharacteristic(
-      unknownServiceCharacteristicUUID
-    );
-
-    // Read the characteristic
-    const unknownServiceValue = await unknownServiceCharacteristic.readValue();
-
-    // Parse the unknownServiceValue
-    console.log(unknownServiceValue);
-
-    // 1. Read the byteLength
-    // console.log(`Length: ${unknownServiceValue.byteLength}`); // Result: 8
-
-    // 2. Create hexadecimal (optional)
-    // Loop the bytes and get the value by index and convert to string
-    // let a = [];
-    // for (let i = 0; i < unknownServiceValue.byteLength; i++) {
-    //     let val = ("00" + unknownServiceValue.getUint8(i).toString(16)).slice(-2);
-    //     a.push(val);
-    // }
-    // console.log(`Length: ${unknownServiceValue.byteLength}`);
-    // console.log(`Name Hex: 0x${a.join("")}`) // result: 0x69506164382c3132
-
-    // 3. Create a textDecoder
-    // https://developer.mozilla.org/en-US/docs/Web/API/TextDecoder
-    // const utf8decoder = new TextDecoder();
-
-    // 4. Convert to an Int8Array ( 8bytes )
-    // const unknownServiceIntArray = new Int8Array(unknownServiceValue.buffer);
-    // console.log(unknownServiceIntArray);
-
-    // 5. Decode the Int8Array to utf8
-    // const unknownService = utf8decoder.decode(unknownServiceIntArray);
-    // console.log(unknownService);
-
-    // WTF is that?
-    // https://gist.github.com/adamawolf/3048717
   },
 };
 
-// User action required to make request
 document.querySelector("#ble-selector").addEventListener("click", () => {
   Bluetooth.init();
 });
